0e10aa2e583b4772ed2fd6a48bee7b855afb944e
cc030 <cc030@Subin>
2023-07-20 21:24:25 +0900

    modified form css

--- project_pond/src/main/webapp/resources/script/validationLogin.js
+++ project_pond/src/main/webapp/resources/script/validationLogin.js
@@ -1,16 +1,14 @@
 $('#id').on('change', e => {
 	if($('#id').val() !== "") {
 		$('#error-id').hide();
-		$('#id').css('border-color', 'lightgrey');
-		$('#id').css('border', 'none');
+		$('#id').css('border-color', '#1E3269');
 	}
 });
 
 $('#pwd').on('change', e => {
 	if($('#pwd').val() !== "") {
 		$('#error-password').hide();
-		$('#pwd').css('border-color', 'lightgrey');
-		$('#pwd').css('border', 'none');
+		$('#pwd').css('border-color', '#1E3269');
 	}
 });
 
@@ -26,13 +24,11 @@ function checkValue(htmlForm) {
 	if(id === "") {
 		$('#error-id').show();
 		$('#id').css('border-color', 'red');
-		$('#id').css('border', 'solid 1px red');
 		check = false;		
 	} 
 	else if(pwd === "") {
 		$('#error-password').show();
 		$('#pwd').css('border-color', 'red');
-		$('#pwd').css('border', 'solid 1px red');
 		check = false;
 	}
 	
